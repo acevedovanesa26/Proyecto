db82aae4b5bbb8720ec62f8bb34368c7e2e30b59
Vanesa <vacevedom1@ucentral.edu.co>
2024-09-26 18:41:40 -0500

    (nuevo) primer commit de estructura

---
+++ src/main/java/co/ucentral/VotosSmart/VotosSmartApplication.java
@@ -0,0 +1,13 @@
+package co.ucentral.VotosSmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotosSmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VotosSmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/ucentral/VotosSmart/VotosSmartApplicationTests.java
@@ -0,0 +1,13 @@
+package co.ucentral.VotosSmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VotosSmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

1bd1b065248e8c74599d8b18840bf130290a74c4
Vanesa <vacevedom1@ucentral.edu.co>
2024-09-29 22:40:23 -0500

    (nuevo): finaliza componentes de consulta todos

--- src/main/java/co/ucentral/VotosSmart/persistencia/repositorios/PropietarioRepositorio.java
+++ src/main/java/co/ucentral/VotosSmart/persistencia/repositorios/PropietarioRepositorio.java
@@ -1,4 +1,9 @@
 package co.ucentral.VotosSmart.persistencia.repositorios;
 
-public interface PropietarioRepositorio {
+
+import co.ucentral.VotosSmart.persistencia.entidades.Propietario;
+import co.ucentral.VotosSmart.persistencia.entidades.Vehiculo;
+import org.springframework.data.repository.CrudRepository;
+
+public interface PropietarioRepositorio extends CrudRepository<Propietario, Long> {
 }
